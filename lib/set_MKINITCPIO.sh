ed0d400c2f1eceb71edd6855ac3fe9bc717bc661
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2022-03-12 19:22:01 +0100

    Support for mkinitcpio

--- lib/set_MKINITCPIO.sh
+++ lib/set_MKINITCPIO.sh
@@ -7,10 +7,13 @@ function set_MKINITCPIO () {
     CURRENTMODULES=$(grep -P "^MODULES" "$SCRIPTDIR/$MKINITCPIO" | perl -pe "s/MODULES=\((.+)\)/\1/")
     MODULES="$(grep -P "^MODULES" "$SCRIPTDIR/$MKINITCPIO" | perl -pe "s/MODULES=\((.+)\)/\1/" | perl -pe "s/\s?(vfio_iommu_type1|vfio_pci|vfio_virqfd|vfio|vendor-reset)\s?//g")"
 
+    # Check if vendor-reset is present
     if [[ $CURRENTMODULES =~ "vendor-reset" ]];
     then
+        # Inject vfio modules with vendor-reset
         perl -pi -e "s/MODULES=\(${CURRENTMODULES}\)/MODULES=\(vendor-reset vfio vfio_iommu_type1 vfio_pci vfio_virqfd ${MODULES}\)/" "$SCRIPTDIR/$MKINITCPIO"
     else
+        # Inject vfio modules
         perl -pi -e "s/MODULES=\(${CURRENTMODULES}\)/MODULES=\(vfio vfio_iommu_type1 vfio_pci vfio_virqfd ${MODULES}\)/" "$SCRIPTDIR/$MKINITCPIO"
     fi
 

f0cd5e408da4231fda6caa934f09680bd0efb185
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-04-10 02:40:50 +0200

    update the generated script

--- internal/configs/config_vbios_dumper.go
+++ internal/configs/config_vbios_dumper.go
@@ -26,15 +26,23 @@ func GenerateVBIOSDumper(vbios_path string) {
 		"#!/bin/bash\n",
 		"# THIS FILE IS AUTO GENERATED!\n",
 		"# IF YOU HAVE CHANGED GPU, PLEASE RE-RUN QUICKPASSTHROUGH!\n",
+		"mkdir -p \"%s\"\n",
+		"echo Attempting to enable reading from rom\n",
 		"echo 1 | sudo tee %s\n",
+		"echo Attempting to dump VBIOS\n",
 		"sudo bash -c \"cat %s\" > %s/%s/vfio_card.rom\n",
+		"file \"%s/%s/vfio_card.rom\"\n",
+		"echo Attempting to disable reading from rom \\(cleanup\\)\n",
 		"echo 0 | sudo tee %s\n",
 	)
 
 	vbios_script := fmt.Sprintf(
 		vbios_script_template,
+		config.Path.QUICKEMU,
 		vbios_path,
 		vbios_path,
+		scriptdir,
+		config.Path.QUICKEMU,
 		scriptdir,
 		config.Path.QUICKEMU,
 		vbios_path,

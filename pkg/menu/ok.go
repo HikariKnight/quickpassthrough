eaf8036330d626116e9ff757f65bc81048ae20eb
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-29 12:08:37 +0100

    add OkBack

--- pkg/menu/ok.go
+++ pkg/menu/ok.go
@@ -2,11 +2,25 @@ package menu
 
 import "github.com/nexidian/gocliselect"
 
-// Make a YesNo menu
+// Make an OK menu
 func Ok(msg string) string {
 	// Make the menu
 	menu := gocliselect.NewMenu(msg)
 	menu.AddItem("OK", "next")
+
+	// Display the menu
+	choice := menu.Display()
+
+	// Return the value selected
+	return choice
+}
+
+// Make an OK & Go Back menu
+func OkBack(msg string) string {
+	// Make the menu
+	menu := gocliselect.NewMenu(msg)
+	menu.AddItem("OK", "next")
+	menu.AddItem("Go Back", "back")
 
 	// Display the menu
 	choice := menu.Display()

4a95fef56be2ce949047e2edfde97b10b771a01c
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-12-26 18:42:49 +0100

    enable new TUI

--- internal/ui_main.go
+++ internal/ui_main.go
@@ -7,25 +7,27 @@ import (
 	"os"
 
 	"github.com/HikariKnight/ls-iommu/pkg/errorcheck"
+	"github.com/HikariKnight/quickpassthrough/internal/pages"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 // This is where we build everything
 func Tui() {
-	// Log all errors to a new logfile
+	// Log all errors to a new logfile (super useful feature of BubbleTea!)
 	os.Remove("debug.log")
 	logfile, err := tea.LogToFile("debug.log", "")
 	errorcheck.ErrorCheck(err, "Error creating log file")
 	defer logfile.Close()
 
 	// New WIP Tui
-	//pages.Welcome()
+	pages.Welcome()
+	/*
+	   // Make a blank model to keep our state in
+	   m := NewModel()
 
-	// Make a blank model to keep our state in
-	m := NewModel()
-
-	// Start the program with the model
-	p := tea.NewProgram(m, tea.WithAltScreen())
-	_, err = p.Run()
-	errorcheck.ErrorCheck(err, "Failed to initialize UI")
+	   // Start the program with the model
+	   p := tea.NewProgram(m, tea.WithAltScreen())
+	   _, err = p.Run()
+	   errorcheck.ErrorCheck(err, "Failed to initialize UI")
+	*/
 }

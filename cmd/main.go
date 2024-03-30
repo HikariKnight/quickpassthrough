9a8e1002cd27e979c1a1a9c487c5cc820a91be41
Matt Spurrier <matthew@senseict.com.au>
2024-03-30 21:24:30 +0800

    update for globally accesible version number

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	internal "github.com/HikariKnight/quickpassthrough/internal"
 	downloader "github.com/HikariKnight/quickpassthrough/internal/ls_iommu_downloader"
 	"github.com/HikariKnight/quickpassthrough/internal/params"
+	"github.com/HikariKnight/quickpassthrough/internal/version"
 )
 
 func main() {
 	// Get all our arguments in 1 neat struct
 	pArg := params.NewParams()
+
+	// Display the version
+	if p.Arg.Flag["version"] {
+		fmt.Printf("QuickPassthrough Version %s\n", version.Version)
+	}
 
 	if !pArg.Flag["gui"] {
 		downloader.CheckLsIOMMU()
